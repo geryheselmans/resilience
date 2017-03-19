5988383bb8aecc05dab4d7ad69b957daef55909d
geryheselmans <gery.heselmans@hotmail.com>
2017-03-19 15:10:19 +0100

    Add fallback scenario + check for timings

--- resilience_test.go
+++ resilience_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TestDoNilFallback(t *testing.T) {
+func TestOneDoSingleResponse(t *testing.T) {
 	var dummyStruct = struct{
 		int
 		string
@@ -14,40 +14,80 @@ func TestDoNilFallback(t *testing.T) {
 		"test",
 	}
 
+	stringFallBack := func(result chan<- interface{}){
+		result <- "Fallback"
+	}
+
+	intFallBack := func(result chan<- interface{}){
+		result <- 456
+	}
+
+	dummyStructFallback := func(result chan<- interface{}){
+		result <- nil
+	}
+
 	var tests = []struct {
 		input interface {}
 		sleep time.Duration
+		fallback func(result chan<- interface{})
 		want  interface{}
 	} {
-		{"Test", 0, "Test"},
-		{"Test", 800, "Test"},
-		{"Test", 1200, nil},
-		{"Test", 1000, nil},
-		{"Test", 998, "Test"},
-		{123, 0, 123},
-		{123, 800, 123},
-		{123, 1200, nil},
-		{123, 1000, nil},
-		{123, 998, 123},
-		{dummyStruct, 0, dummyStruct},
-		{dummyStruct, 800, dummyStruct},
-		{dummyStruct, 1200, nil},
-		{dummyStruct, 1000, nil},
-		{dummyStruct, 998, dummyStruct},
+		{"Test", 0, nil, "Test"},
+		{"Test", 800, nil,"Test"},
+		{"Test", 1200, nil,nil},
+		{"Test", 1002, nil,nil},
+		{"Test", 998, nil,"Test"},
+		{"Test", 0, stringFallBack, "Test"},
+		{"Test", 800, stringFallBack,"Test"},
+		{"Test", 1200, stringFallBack,"Fallback"},
+		{"Test", 1002, stringFallBack,"Fallback"},
+		{"Test", 998, stringFallBack,"Test"},
+		{123, 0, nil,123},
+		{123, 800, nil,123},
+		{123, 1200, nil,nil},
+		{123, 1002, nil,nil},
+		{123, 998, nil,123},
+		{123, 0, intFallBack,123},
+		{123, 800, intFallBack,123},
+		{123, 1200, intFallBack,456},
+		{123, 1002, intFallBack,456},
+		{123, 998, intFallBack,123},
+		{dummyStruct, 0, nil,dummyStruct},
+		{dummyStruct, 800,nil, dummyStruct},
+		{dummyStruct, 1200, nil,nil},
+		{dummyStruct, 1002, nil,nil},
+		{dummyStruct, 998, nil,dummyStruct},
+		{dummyStruct, 0, dummyStructFallback,dummyStruct},
+		{dummyStruct, 800,dummyStructFallback, dummyStruct},
+		{dummyStruct, 1200, dummyStructFallback,nil},
+		{dummyStruct, 1002, dummyStructFallback,nil},
+		{dummyStruct, 998, dummyStructFallback,dummyStruct},
 	}
 
 	for _,test := range tests {
+		start := time.Now()
 		result := Do(func(result chan<- interface{}){
 
 			time.Sleep(test.sleep * time.Millisecond)
 
 			result <- test.input
-		}, nil)
+		}, test.fallback)
+
 
 		got := <- result
+		stop:= time.Now()
+
+		diff := stop.Sub(start)
+
+		maxDiff := (test.sleep * time.Millisecond) + (5 * time.Millisecond)
+
+		if diff > maxDiff {
+			t.Errorf("%q @ %q: got %q must be max %q",
+				test.input, test.sleep * time.Millisecond, diff , maxDiff)
+		}
 
 		if got != test.want {
-			t.Errorf("%s @ %q: got %q, want %q,",
+			t.Errorf("%q @ %q: got %q, want %q,",
 				test.input, test.sleep * time.Millisecond, got, test.want)
 		}
 	}
