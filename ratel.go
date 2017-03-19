c80c78f9df6dce3d286983bbde1d09f08c435f37
geryheselmans <gery.heselmans@hotmail.com>
2017-03-19 13:28:14 +0100

    Fixed deadlock bug must run in a goroutine

--- ratel.go
+++ ratel.go
@@ -2,21 +2,32 @@ package ratel
 
 import "time"
 
-func Do(command func(chan<- interface{})) chan interface{} {
+func Do(command func(chan<- interface{}), fallback func(chan<- interface{})) chan interface{} {
 	returnChan := make(chan interface{})
+
+	go executeCommand(returnChan, command, fallback)
+
+	return returnChan
+}
+
+func executeCommand(returnChan chan<- interface{},command func(chan<- interface{}), fallback func(chan<- interface{})){
 	commandChan := make(chan interface{})
+	go command(commandChan)
 
 	timeOut := time.NewTimer(time.Second)
-
-	go command(commandChan)
 
 	select {
 	case result := <- commandChan:
 		returnChan <- result
 	case <- timeOut.C:
-		returnChan <- nil
+		if fallback != nil {
+			fallbackChan := make(chan interface{})
 
+			go fallback(fallbackChan)
+
+			returnChan <- <- fallbackChan
+		} else {
+			returnChan <- nil
+		}
 	}
-
-	return returnChan
 }
