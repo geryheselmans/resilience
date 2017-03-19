25900338879b97f5dc26f70bb8fe94b2b4b253c9
geryheselmans <gery.heselmans@hotmail.com>
2017-03-19 13:28:58 +0100

    Added test scenarios

--- ratel_test.go
+++ ratel_test.go
@@ -6,14 +6,34 @@ import (
 )
 
 func TestDoNilFallback(t *testing.T) {
-	var tests = []struct {
-		toReturn string
-		sleep time.Duration
-		expected interface{}
+	var dummyStruct = struct{
+		int
+		string
 	} {
-		//{"Test", 0, "Test"},
+		123,
+		"test",
+	}
+
+	var tests = []struct {
+		input interface {}
+		sleep time.Duration
+		want  interface{}
+	} {
+		{"Test", 0, "Test"},
+		{"Test", 800, "Test"},
 		{"Test", 1200, nil},
-		{"Test", 1000, "Test"},
+		{"Test", 1000, nil},
+		{"Test", 998, "Test"},
+		{123, 0, 123},
+		{123, 800, 123},
+		{123, 1200, nil},
+		{123, 1000, nil},
+		{123, 998, 123},
+		{dummyStruct, 0, dummyStruct},
+		{dummyStruct, 800, dummyStruct},
+		{dummyStruct, 1200, nil},
+		{dummyStruct, 1000, nil},
+		{dummyStruct, 998, dummyStruct},
 	}
 
 	for _,test := range tests {
@@ -21,13 +41,14 @@ func TestDoNilFallback(t *testing.T) {
 
 			time.Sleep(test.sleep * time.Millisecond)
 
-			result <- test.toReturn
+			result <- test.input
 		}, nil)
 
-		actual := <- result
+		got := <- result
 
-		if actual != test.expected {
-			t.Errorf("Result not correct, want %s, got %s", actual, result)
+		if got != test.want {
+			t.Errorf("%s @ %q: got %q, want %q,",
+				test.input, test.sleep * time.Millisecond, got, test.want)
 		}
 	}
 }
